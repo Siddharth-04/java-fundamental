d68367902165b4be10f5a7ffee2a17d030425a5d
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-02-27 13:28:11 +0530

    [Siddharth] ADD: Calculate Power of a number Java Program

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PerimeterOfRectangle{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the length of rectangle : ");
+		int length = sc.nextInt();
+		System.out.print("Enter the breadth of rectangle : ");
+		int breadth = sc.nextInt();
+		int perimeter = 2*(length+breadth);
+		System.out.print("Perimeter of Rectangle is  : " + perimeter);
+	}
+}
