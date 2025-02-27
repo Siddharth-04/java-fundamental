ca30b83565c6d6afe0cce4b40c380008af88a5cc
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-02-27 13:17:30 +0530

    [Siddharth] ADD : Addition of Two Numbers Java Program

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class AddTwoNumbers{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first number : ");
 		int a = sc.nextInt();
+		System.out.print("Enter the second number : ");
 		int b = sc.nextInt();
 		int c = a+b;
 		System.out.print("Addition of two numbers is : " + c);
